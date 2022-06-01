bcb21c9f605a64fc8e5b2625d249eb2f7bd7b421
Aleksandr Lobkov <das149848@gmail.com>
2022-06-01 05:14:51 +0400

    refactor: change one class so that it applies to BEM rules

--- app.js
+++ app.js
@@ -86,11 +86,11 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".button_edit");
-    var containsClass=listItem.classList.contains("editMode");
-    //If class of the parent is .editmode
+    var containsClass=listItem.classList.contains("item_edit-mode");
+    //If class of the parent is .item_edit-mode
     if(containsClass){
 
-        //switch to .editmode
+        //switch to .item_edit-mode
         //label becomes the inputs value.
         label.innerText=editInput.value;
         editBtn.innerText="Edit";
@@ -99,8 +99,8 @@ var editTask=function(){
         editBtn.innerText="Save";
     }
 
-    //toggle .editmode on the parent.
-    listItem.classList.toggle("editMode");
+    //toggle .item_edit-mode on the parent.
+    listItem.classList.toggle("item_edit-mode");
 };
 
 
